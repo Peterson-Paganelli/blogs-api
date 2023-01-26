d1093fb4d2ae667a029b89359e14b6e652532969
Peterson <petersonpaganelli@outlook.com>
2023-01-26 17:14:04 -0300

    Requisito 2

---
+++ src/models/User.js
@@ -0,0 +1,12 @@
+const UserModel = (sequelize, Datatypes) => {
+  const User  = sequelize.define('User', {
+    id: Datatypes.INTEGER,
+    display_name: Datatypes.STRING,
+    email: Datatypes.STRING,
+    password: Datatypes.STRING,
+    image: Datatypes.STRING,
+  })
+  return User;
+};
+
+module.exports = UserModel;
